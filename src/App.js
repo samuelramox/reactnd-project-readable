00b0d318157e8848d80c35312c4aee6622fbce5a
Samuel Ramos <samramox@gmail.com>
2018-06-25 16:43:49 -0300

    Updated App.js with axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount = () => {
+    const url = 'http://localhost:3001/posts';
+    const headers = { Authorization: 'whatever-you-want' };
+    axios({
+      method: 'get',
+      url,
+      headers
+    }).then(res => res.data);
+  };
+
   render() {
     return <div />;
   }
