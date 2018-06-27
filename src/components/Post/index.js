3fd270f072e7c362b58937e75d5ec4e02c6578f7
Samuel Ramos <samramox@gmail.com>
2018-06-27 10:47:34 -0300

    Added post

---
+++ src/components/Post/index.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { commentsFetchData } from '../../actions/comments';
+import { postFetchById } from '../../actions/posts';
+import Comments from '../Comments';
+
+class Post extends PureComponent {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+    this.props.fetchComments(id);
+  }
+
+  render() {
+    const { post, c_isLoading, p_isLoading, comments } = this.props;
+    if (p_isLoading) {
+      return <h1>Loading</h1>;
+    }
+    return (
+      <article>
+        <h1>{post.title}</h1>
+        <div>{post.body}</div>
+        <Comments isLoading={c_isLoading} data={comments} />
+      </article>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    comments: state.comments,
+    c_hasErrored: state.commentsHasErrored,
+    c_isLoading: state.commentsIsLoading,
+    p_hasErrored: state.postsHasErrored,
+    p_isLoading: state.postsIsLoading,
+    post: state.post
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchComments: idPost => dispatch(commentsFetchData(idPost)),
+  fetchPost: idPost => dispatch(postFetchById(idPost))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
