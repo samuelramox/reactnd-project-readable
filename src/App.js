fab07cd14965e249cf297ad59d5061cc70c5a015
Samuel Ramos <samramox@gmail.com>
2018-06-27 10:51:29 -0300

    Updated App with routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Categories from './components/Categories';
+import NotFound from './components/NotFound';
+import Post from './components/Post';
+import Posts from './components/Posts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={} exact />
-        <Route path="/" component={} exact />
+        <Switch>
+          <Route exact path="/" component={Posts} />
+          <Route path="/posts/:id" component={Post} />
+          <Route path="/categories" component={Categories} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
