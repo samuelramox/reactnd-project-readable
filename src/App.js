4cbbfe85999a4d987438a2af0e54cbac34c8eb98
Samuel Ramos <samramox@gmail.com>
2018-06-28 14:37:33 -0300

    Refactor: changed Handle to HandlePost

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Categories from './components/Categories';
-import Handle from './components/Handle';
+import HandlePost from './components/HandlePost';
 import NotFound from './components/NotFound';
 import Post from './components/Post';
 import Posts from './components/Posts';
@@ -15,8 +15,8 @@ class App extends Component {
           <Route path="/categories" component={Categories} />
           <Route path="/:category" component={Posts} />
           <Route path="/:category/:id" component={Post} />
-          <Route path="/admin/post" component={Handle} />
-          <Route path="/admin/post/:id" component={Handle} />
+          <Route path="/admin/post" component={HandlePost} />
+          <Route path="/admin/post/:id" component={HandlePost} />
           <Route component={NotFound} />
         </Switch>
       </div>
