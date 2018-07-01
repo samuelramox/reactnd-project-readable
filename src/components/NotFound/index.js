1e20328cc5fbef33ab47d6f91b880547a405f904
Samuel Ramos <samramox@gmail.com>
2018-07-01 00:11:52 -0300

    Feat: added new functions

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { HashLoader, SyncLoader } from 'react-spinners';
 
-const NotFound = () => {
+export const Error = () => {
   return (
-    <div>
-      <h1>Page not found!</h1>
+    <div className="d-flex flex-column align-items-center text-secondary">
+      <h1 className="my-5">Error fetching data</h1>
+      <HashLoader color="#F8344C" />
     </div>
   );
 };
 
-export default NotFound;
+export const Loading = () => {
+  return (
+    <div className="d-flex flex-column align-items-center text-secondary">
+      <h1 className="my-5">Loading...</h1>
+      <SyncLoader color="#F8344C" />
+    </div>
+  );
+};
+
+export const PageNotFound = () => {
+  return (
+    <div className="d-flex flex-column align-items-center text-secondary">
+      <h1 className="my-5">Page not found!</h1>
+      <HashLoader color="#F8344C" />
+    </div>
+  );
+};
