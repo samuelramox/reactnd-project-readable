4893ddbfae89f7bed61ef246af7b453cca3f0cf8
Samuel Ramos <samramox@gmail.com>
2018-07-06 11:47:39 -0300

    Fix: fetchError status

--- src/components/HandlePost/index.js
+++ src/components/HandlePost/index.js
@@ -5,7 +5,7 @@ import serializeForm from 'form-serialize';
 import swal from 'sweetalert2';
 import uuidv1 from 'uuid/v1';
 import { Button, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
-import { PageNotFound } from '../NotFound';
+import { Error } from '../NotFound';
 import { postFetchById, insertPost, updatePost } from '../../actions/posts';
 import { categoriesFetchData } from '../../actions/categories';
 
@@ -94,7 +94,7 @@ class HandlePost extends Component {
     const command = update.isUpdate === true ? 'Update' : 'Create';
 
     if (fetchError) {
-      return <PageNotFound />;
+      return <Error />;
     }
 
     return (
